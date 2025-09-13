a1f17e7038b54bd6e5cd590fb01ec7faa6197eba
x3alone <otmanchouarix3@gmail.com>
2025-09-13 06:42:11 +0100

    UI

---
+++ frontend-01blog/src/app/about/about.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  standalone: true,
+  template: `<h1>About Page</h1>`,
+})
+export class AboutComponent {}
--- frontend-01blog/src/app/app.ts
+++ frontend-01blog/src/app/app.ts
@@ -3,10 +3,12 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  // imports: [RouterOutlet], // uncomment when routing added
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrls: ['./app.scss']
 })
 export class App {
   protected readonly title = signal('frontend-01blog');
 }
+
